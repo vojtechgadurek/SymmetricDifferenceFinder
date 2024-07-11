3ebf510777f801da61cb3c7f37d1d226c4dd32f3
Vojtech Gadurek <vg.vojtawar@outlook.com>
2024-07-11 06:52:17 +0200

    change benchmarks

--- SymmetricDifferenceBenchmarks/EncoderBenchmarks.cs
+++ SymmetricDifferenceBenchmarks/EncoderBenchmarks.cs
@@ -30,7 +30,7 @@ namespace SymmetricDifferenceFinderBenchmarks
 
 
 			public static IEnumerable<ulong> TableLengths()
-				=> new ulong[] { 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576 };
+				=> Enumerable.Range(10, 27).Select(x => 1ul << x);
 
 			readonly public Random random = new Random();
 
